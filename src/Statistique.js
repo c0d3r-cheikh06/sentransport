6273bf9ce841a7030727db3d434f03bb1ed6df56
Cheikh <chkthiam06@gmail.com>
2026-05-01 16:01:54 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,10 @@
+function Statistique() {
+  return (
+    <div className="statistique">
+      <span className="statistique-chiffre">10 </span>
+      <span className="statistique-libelle">lignes</span>
+    </div>
+  );
+}
+
+export default Statistique;
---
+++ src/Statistique3.js
@@ -0,0 +1,10 @@
+function Statistique3() {
+  return (
+    <div className="statistique">
+      <span className="statistique-chiffre">32 </span>
+      <span className="statistique-libelle">bus en service</span>
+    </div>
+  );
+}
+ 
+export default Statistique3;
