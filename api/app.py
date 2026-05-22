fe9dca752145ed10ae39b5ea8a091d35e805c022
Cheikh <chkthiam06@gmail.com>
2026-05-22 00:50:19 +0000

    Lab 5 : React consomme Flask via fetch et useEffect

--- api/app.py
+++ api/app.py
@@ -37,4 +37,12 @@ def get_ligne(ligne_id):
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-    
+
+@app.route("/arrets")
+def get_arrets():
+    tous_les_arrets = set()
+    for ligne in lignes:
+        for arret in ligne["listeArrets"]:
+            tous_les_arrets.add(arret)
+    return jsonify(list(tous_les_arrets))
+
